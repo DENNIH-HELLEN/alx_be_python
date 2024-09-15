70ca823b22f43ceb49db836b5553b947dda063bb
“Dennis <mwangidennis771414@gmail.com>
2024-09-15 07:03:32 -0700

    Add bracket

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -6,7 +6,7 @@ m2 = int(input("Enter your monthly expenses: "))
 
 m3 = m1 - m2
 
-ps = m3 * 12 + (m3 * 12 * 0.05)
+ps = (m3 * 12 + (m3 * 12 * 0.05))
 
 print("Your monthly savings are ${0:d}.".format(m3))
 
