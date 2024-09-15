61b2cfcb17c2bb36993672dc37926bbf3136d0d6
“Dennis <mwangidennis771414@gmail.com>
2024-09-15 06:59:07 -0700

    task 5

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+m1 = int(input("Enter your monthly income: "))
+
+m2 = int(input("Enter your monthly expenses: "))
+
+m3 = m1 - m2
+
+ps = m3 * 12 + (m3 * 12 * 0.05)
+
+print("Your monthly savings are ${0:d}.".format(m3))
+
+print("Projected savings after one year, with interest, is: ${0:.0f}.".format(ps))
+
