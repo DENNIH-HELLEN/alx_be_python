042cd234dd50c3c331e54ea5042154fcd8d40bbe
Dennis Mwangi <mwangidennis771414@gmail>
2024-09-15 04:42:35 -0700

    New project Task 0

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+number1 = 10
+
+number2 = 5
+
+sum_1 = (number1 + number2)
+
+min_1 = (number1 - number2)
+mult = (number1 * number2)
+print("Addition of {0:d} and {1:d} is {2:d}".format(number1, number2, sum_1))
+
+print("Subtraction of {0:d} and {1:d} is {2:d}".format(number1, number2, min_1))
+
+print("Multiplication of {0:d} and {1:d} is {2:d}".format(number1, number2, mult))
