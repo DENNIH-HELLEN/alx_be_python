9fcc78ff7eecc1f633882e31cade9ab35a3d01f6
Dennis Mwangi <mwangidennis771414@gmail.com>
2024-09-25 16:18:10 +0300

    fix task 2

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,6 +2,5 @@
 num1 = int(input("Enter a number to see its multiplication table: "))
 
 for i in range (1, 11):
-    answer = num1 * i
-    print("{0} * {1} = {2}".format(num1, i, answer))
+    print(f"{num1} * {i} = {num1 * i}")
     
