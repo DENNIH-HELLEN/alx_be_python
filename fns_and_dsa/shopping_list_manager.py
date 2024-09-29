9656796879da9f331526ad0814762e024ba59619
Dennis Mwangi <mwangidennis771414@gmail.com>
2024-09-29 18:05:45 +0300

    alx

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,44 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            add_item = input("Enter item to be added: ")
+            shopping_list.append(add_item)
+            
+        elif choice == '2':
+            # Prompt for and remove an item
+            remove_item = input("Enter item to be removed: ")
+            
+            try:
+                shopping_list.remove(remove_item)
+                
+            except ValueError:
+                print(f"{remove_item} is not in the list.")
+                
+        elif choice == '3':
+            # Display the shopping list
+            for x in range(len(shopping_list)):
+                print(shopping_list[x])
+                
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+            
+
+if __name__ == "__main__":
+    main()
+
+    
