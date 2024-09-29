f74375a384da4b574145e084d2cb681ee36f6644
Dennis Mwangi <mwangidennis771414@gmail.com>
2024-09-29 17:33:07 +0300

    task 3

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,12 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
+
+def convert_to_celsius(fahrenheit):
+    ans = ( fahrenheit - 32 ) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return ans 
+    
+def convert_to_fahrenheit(celsius):
+   ans = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+   return ans 
