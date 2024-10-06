87b57dec81606c06626de7f64811fcb0ed8e1103
Dennis Mwangi <mwangidennis771414@gmail.com>
2024-10-06 17:04:53 +0300

    fix print

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -10,4 +10,4 @@ def safe_divide (numerator, denominator):
         return "Error: Please enter numeric values only."
 
     else:
-        return ans
+        return "The result of the division is {ans}"
