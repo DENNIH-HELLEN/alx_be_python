76031687177bd52ed47e6d35bd1b3ee28512ad7b
Dennis Mwangi <mwangidennis771414@gmail.com>
2024-10-06 17:03:16 +0300

    task 1

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,13 @@
+def safe_divide (numerator, denominator):
+    try:
+        n = float(numerator)
+        d = float(denominator)
+        ans = n / d
+        
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero."
+    except ValueError:
+        return "Error: Please enter numeric values only."
+
+    else:
+        return ans
