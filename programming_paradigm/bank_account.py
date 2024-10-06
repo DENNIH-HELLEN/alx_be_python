d19303bf91bfe6904e01d70c15349e4f73492ee3
Dennis Mwangi <mwangidennis771414@gmail.com>
2024-10-06 16:33:36 +0300

    Task 0

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__ (self, initial_balance=0):
+        self.account_balance = float(initial_balance)
+        
+    def deposit(self, amount):
+        self.account_balance += amount
+        
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= float(amount)
+            return True
+        else:
+            return False
+            
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
+        return self.account_balance
